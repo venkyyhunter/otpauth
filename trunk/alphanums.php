016765328743c80a9bd088e2428c060a83fd33e8
james.barkley <james.barkley@f9940d4a-9f55-0410-9c0b-ef6443a9687f>
2008-08-29 20:48:45 +0000

    initial commit

---
+++ trunk/alphanums.php
@@ -0,0 +1,99 @@
+<?php
+	/* **************************************************************************************
+	* CODE            : alphanums.php
+	* LAST UPDATED    : July 2007 by james.barkley@gmail.com
+	* METHOD          : include/require/inline
+	*
+	* DESCRIPTION     : This piece of code defines an ordered array called alphanums that
+	*        contain all upper and lower alphabet characters (standard english
+	*        26 letter alphabet) and the digits 0-9.
+	*
+	* LICENSE     : GPL
+	*
+	************************************************************************************** */
+	$alphanums[] = "a";
+	$alphanums[] = "b";
+	$alphanums[] = "c";
+	$alphanums[] = "d";
+	$alphanums[] = "e";
+	$alphanums[] = "f";
+	$alphanums[] = "g";
+	$alphanums[] = "h";
+	$alphanums[] = "i";
+	$alphanums[] = "j";
+	$alphanums[] = "k";
+	$alphanums[] = "l";
+	$alphanums[] = "m";
+	$alphanums[] = "n";
+	$alphanums[] = "o";
+	$alphanums[] = "p";
+	$alphanums[] = "q";
+	$alphanums[] = "s";
+	$alphanums[] = "t";
+	$alphanums[] = "u";
+	$alphanums[] = "v";
+	$alphanums[] = "w";
+	$alphanums[] = "x";
+	$alphanums[] = "y";
+	$alphanums[] = "z";
+	$alphanums[] = "A";
+	$alphanums[] = "B";
+	$alphanums[] = "C";
+	$alphanums[] = "D";
+	$alphanums[] = "E";
+	$alphanums[] = "F";
+	$alphanums[] = "G";
+	$alphanums[] = "H";
+	$alphanums[] = "I";
+	$alphanums[] = "J";
+	$alphanums[] = "K";
+	$alphanums[] = "L";
+	$alphanums[] = "M";
+	$alphanums[] = "N";
+	$alphanums[] = "O";
+	$alphanums[] = "P";
+	$alphanums[] = "Q";
+	$alphanums[] = "R";
+	$alphanums[] = "S";
+	$alphanums[] = "T";
+	$alphanums[] = "U";
+	$alphanums[] = "V";
+	$alphanums[] = "W";
+	$alphanums[] = "X";
+	$alphanums[] = "Y";
+	$alphanums[] = "Z";
+	$alphanums[] = "0";
+	$alphanums[] = "1";
+	$alphanums[] = "2";
+	$alphanums[] = "3";
+	$alphanums[] = "4";
+	$alphanums[] = "5";
+	$alphanums[] = "6";
+	$alphanums[] = "7";
+	$alphanums[] = "8";
+	$alphanums[] = "9";
+?>
+<?php
+	/*
+	-----BEGIN PGP MESSAGE-----
+	Version: GnuPG v1.2.1 (GNU/Linux)
+	 
+	hQIOA6jz1R+atzBZEAf9EJXPCKrk9SPzLNALLY61g9oxSIq/eExVKDkTeTed9y5p
+	HV37pp2tJXBoT5kNJ46Y2/xd1tDmfnSCD/cOVcjOafis7eGKElR39o4VXFi6ToqN
+	w7RKZyY6TvL+uaFkNTHED4oSFRH7IYK9zxHBKoiO9qAcWBSaoMMgA1uyBoDvM6Hf
+	+1TpyAO3I10KG50/rdWVLXWLaNaPk+dhmx7s965k9wrmYV6fdH79P1E/Z7grjv8o
+	oPbNnF0PSruSc42ZTk4nfoMHI5ORJ5qAtN0BfWsFm+/zuwCci/zdNWDyY/7jrJWN
+	lc2We5rVF4F6OLmKXUWWIIS2okAorIRmZt9Xc/e7Zgf9HtMZeJAJgHoLRgV9IHuw
+	9Ul6hqk2634UpcIiRwfMU/0towB8qGRmm6f8ZEGy8FsYvdlO7JmRinVTY5RulhPD
+	R8LWN/Y6kUd5DnS2ddfn75WrUuInTBtq0LKpLPZt6dZimdqO4skCxC2qkwVGEtjH
+	hgyH9ItonQPD8Wkp2KliTq3D7NeTDfYC1fEkGzO2avIjue80mA0yfzCW1UNk2wd3
+	kvHTicScLi40sUq57nKPZc4EM0/KngegNjSFNRuunN5iJ0Y4cvRs8bh/toLfcrQi
+	7DxDs9BQMcPcbXSWoWYYwjWkcgI/mYAEKNnaegqG2NUh+yXJzhUSZsEnCLYEsKUg
+	vtKZAb5HIw8MWdSSWzBvmzVDBX7TnXtwQVHfUTtZQZ17ZHE1pzKhSYQCOG/4OcFa
+	EVMhXzyPRJ+DyrnZc85/5vrpAYNE1GLYatSb2jky2c4Alguk8KjzYvlZtDbnn8hs
+	LgDvOA4zFzaVdEwlqJYzdRA5cEDDMDPWUaXgGYcfUoacp41fLPv9uWb/RHlaR4Ku
+	ArepJGt9N5/Ztc3i
+	=YHhe
+	-----END PGP MESSAGE-----
+	*/
+?>
