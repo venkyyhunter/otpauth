57f3ba5a1fb32f4814c95d998f8fc6db2bff73a0
james.barkley <james.barkley@f9940d4a-9f55-0410-9c0b-ef6443a9687f>
2008-12-08 04:04:01 +0000

    - added function documentation in otp - deprecated unused function - renamed function - added sqllite integration into demoapp - added table structure to flat sql create statements in demo app

--- trunk/demo/index.php
+++ trunk/demo/index.php
@@ -1,5 +1,7 @@
 <?php
 
+initialize_db();
+
 print "Please login with <b>user 'demo'</b> and <b>password 'demopass'</b>.";
 
 
@@ -10,6 +12,7 @@ print "<form>";
 
 print "Username: <input type='text' name='user' value='demo'>";
 print "<br/>";
+print "<br/>";
 print "Password: <input type='text' name='password' value=''>";
 print "<br/>";
 print "<br/>";
@@ -19,4 +22,18 @@ print "<input type='submit' name='login'>";
 print "</form>";
 
 
+?>
+<?php
+
+function initialize_db() {
+	$dbhandle = sqlite_open('demodb.sqllite');
+
+
+	$query = sqlite_exec($dbhandle, "CREATE TABLE test (id int(11), requests int(11), PRIMARY KEY (id))", $error);
+	if (!$query) {
+		exit("Error in query: '$error'");
+	} else {
+		echo 'Number of rows modified: ', sqlite_changes($dbhandle);
+	}
+}
 ?>
