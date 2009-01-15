fa6bc0e43ed3290941a7175531521caebc66acd0
james.barkley <james.barkley@f9940d4a-9f55-0410-9c0b-ef6443a9687f>
2009-01-15 04:31:31 +0000

    beautified

--- trunk/tests/ivcs_transform_test.php
+++ trunk/tests/ivcs_transform_test.php
@@ -1,129 +1,127 @@
-<html>
-<body>
-<h1>ivcs_transform_test.php</h1>
+< html >
+< body >
+< h1 > ivcs_transform_test.php < /h1 >
 <?php
-
-require_once('../otp.php'); 
-
-echo "Demonstration of correct checksum (for values shown in RFC2289): <BR>";
-
-$in[0] = "85c43ee03857765b";
-
-$result = ivcs_transform_to($in[0]);
-echo "ivcs_transform_to($in[0]) = ";
-echo implode(",",$result),"<BR>";
-
-$invert = ivcs_transform_from($result);
-if($invert == false ) echo "Unexpected checksum error ! Something is wrong.<BR>";
-$printres =implode(",",$result);
-echo "ivcs_transform_from($printres) = ", $invert,"<BR>";
-echo "Last codeword should be OAF <BR>";
-
-
-
-$in[0]  = "ff11EE22DD33CC44";
-$in[1]  = "0123456789AbcDef";
-$in[2]  = "10fe32dc54ba7694";
-$in[3]  = "1f11EE22DD33CC44";
-$in[4]  = "2123456789AbcDef";
-$in[5]  = "30fe32dc54ba7694";
-$in[6]  = "4f11EE22DD33CC44";
-$in[7]  = "5123456789AbcDef";
-$in[8]  = "60fe32dc54ba7694";
-$in[9]  = "7f11EE22DD33CC44";
-$in[10] = "8123456789AbcDef";
-$in[11] = "90fe32dc54ba7694";
-
-$i =0;
-foreach($in as $x)
-{
-   $converted[$i++] = ivcs_transform_to($x);
-}
-
-
-echo "<BR>";
-echo "Testing Inverses:   testHexData == ivcs_transform_from(ivcs_transform_to(testHexData)))  <BR>";
-
-$ptr = 0;
-$successCount = 0;
-$failureCount = 0;
-foreach($converted as $x)
-{
-
-    $original = strtolower($in[$ptr]);
-    $transformToAndFrom = ivcs_transform_from($x);
-    if($transformToAndFrom == false)
-    {  
-       echo "Unexpected checksum error ! Something is wrong.<BR>";
-       echo "Input = ", $in[$ptr],"<BR>";
-       echo "Code Words = " , implode(",",$x), " <BR>";
+     
+    require_once('../otp.php');
+     
+    echo "Demonstration of correct checksum (for values shown in RFC2289): <BR>";
+     
+    $in[0] = "85c43ee03857765b";
+     
+    $result = ivcs_transform_to($in[0]);
+    echo "ivcs_transform_to($in[0]) = ";
+    echo implode(",", $result), "<BR>";
+     
+    $invert = ivcs_transform_from($result);
+    if ($invert == false ) echo "Unexpected checksum error ! Something is wrong.<BR>";
+    $printres = implode(",", $result);
+    echo "ivcs_transform_from($printres) = ", $invert, "<BR>";
+    echo "Last codeword should be OAF <BR>";
+     
+     
+     
+    $in[0] = "ff11EE22DD33CC44";
+    $in[1] = "0123456789AbcDef";
+    $in[2] = "10fe32dc54ba7694";
+    $in[3] = "1f11EE22DD33CC44";
+    $in[4] = "2123456789AbcDef";
+    $in[5] = "30fe32dc54ba7694";
+    $in[6] = "4f11EE22DD33CC44";
+    $in[7] = "5123456789AbcDef";
+    $in[8] = "60fe32dc54ba7694";
+    $in[9] = "7f11EE22DD33CC44";
+    $in[10] = "8123456789AbcDef";
+    $in[11] = "90fe32dc54ba7694";
+     
+    $i = 0;
+    foreach($in as $x) {
+        $converted[$i++] = ivcs_transform_to($x);
     }
-    $transformToAndFrom = strtolower($transformToAndFrom);
-    echo $original;
-    if(strcmp(strtolower($transformToAndFrom),strtolower($in[$ptr]))==0)
-    {
-       echo "  (passed) <BR>";
-       $successCount++;
+     
+     
+    echo "<BR>";
+    echo "Testing Inverses:   testHexData == ivcs_transform_from(ivcs_transform_to(testHexData)))  <BR>";
+     
+    $ptr = 0;
+    $successCount = 0;
+    $failureCount = 0;
+    foreach($converted as $x) {
+         
+        $original = strtolower($in[$ptr]);
+        $transformToAndFrom = ivcs_transform_from($x);
+        if ($transformToAndFrom == false) {
+            echo "Unexpected checksum error ! Something is wrong.<BR>";
+            echo "Input = ", $in[$ptr], "<BR>";
+            echo "Code Words = " , implode(",", $x), " <BR>";
+        }
+        $transformToAndFrom = strtolower($transformToAndFrom);
+        echo $original;
+        if (strcmp(strtolower($transformToAndFrom), strtolower($in[$ptr])) == 0) {
+            echo "  (passed) <BR>";
+            $successCount++;
+        } else {
+            echo "<BR>";
+            echo $transformToAndFrom, "<BR>";
+            echo "^^^^^FAILED^^^^^^^^<BR>";
+            $failureCount++;
+        }
+        $ptr++;
     }
-    else
-    {
-        echo "<BR>";
-        echo $transformToAndFrom,"<BR>";
-        echo "^^^^^FAILED^^^^^^^^<BR>";
-       $failureCount++;
+    echo $failureCount , "  failures, ", $successCount, " successes, out of ", $ptr, " total <BR>";
+     
+     
+    $iterations = 40;
+     
+    //--TIMING ivcs_transform_from------------------------------------------------------
+     
+    echo "<BR>";
+    echo "Timing ivcs_transform_from()<BR>";
+    ////////////////////////////////////////////////////
+    $time1 = time();
+    $counter = 0;
+    for($i = 0; $i < $iterations; $i++) {
+        foreach($converted as $codewords) {
+            ///////////////////////////////////////////////////
+             
+             
+            $result = ivcs_transform_from($codewords);
+             
+             
+            ///////////////////////////////////////////////////
+            $counter++;
+        }
     }
-    $ptr++;
-}
-echo $failureCount , "  failures, ",$successCount," successes, out of ",$ptr," total <BR>";
-
-
-$iterations = 40;
-
-//--TIMING ivcs_transform_from------------------------------------------------------
-
-echo "<BR>";
-echo "Timing ivcs_transform_from()<BR>";
-////////////////////////////////////////////////////
-$time1 = time();
-$counter = 0;
-for($i = 0; $i < $iterations; $i++){foreach($converted as $codewords){
-///////////////////////////////////////////////////
-
-
-$result = ivcs_transform_from($codewords);
-
-
-///////////////////////////////////////////////////
-$counter++;
-}}
-$time2=time();
-echo "elapsed time = ", $time2 - $time1 ," seconds.<BR>";
-echo "For $counter six-word code groups converted.<BR>";
-//////////////////////////////////////////////////
-
-
-
-//--TIMING ivcs_transform_to------------------------------------------------------
-
-echo "<BR>";
-echo "Timing ivcs_transform_to()<BR>";
-////////////////////////////////////////////////////
-$time1 = time();
-$counter = 0;
-for($i = 0; $i < $iterations; $i++){foreach($in as $hex){
-///////////////////////////////////////////////////
-
-
-$result = ivcs_transform_to($hex);
-
-
-///////////////////////////////////////////////////
-$counter++;
-}}
-$time2=time();
-echo "elapsed time = ", $time2 - $time1 ," seconds.<BR>";
-echo "For $counter hex values converted.<BR>";
-
+    $time2 = time();
+    echo "elapsed time = ", $time2 - $time1 , " seconds.<BR>";
+    echo "For $counter six-word code groups converted.<BR>";
+    //////////////////////////////////////////////////
+     
+     
+     
+    //--TIMING ivcs_transform_to------------------------------------------------------
+     
+    echo "<BR>";
+    echo "Timing ivcs_transform_to()<BR>";
+    ////////////////////////////////////////////////////
+    $time1 = time();
+    $counter = 0;
+    for($i = 0; $i < $iterations; $i++) {
+        foreach($in as $hex) {
+            ///////////////////////////////////////////////////
+             
+             
+            $result = ivcs_transform_to($hex);
+             
+             
+            ///////////////////////////////////////////////////
+            $counter++;
+        }
+    }
+    $time2 = time();
+    echo "elapsed time = ", $time2 - $time1 , " seconds.<BR>";
+    echo "For $counter hex values converted.<BR>";
+     
 ?>
 </body>
 </html>
--- trunk/tests/otpauthHtmlReporter.class.php
+++ trunk/tests/otpauthHtmlReporter.class.php
@@ -1,58 +1,57 @@
 <?php
-if (! defined('SIMPLE_TEST')) {
-  define('SIMPLE_TEST', './simpletest/');
-}
-
-require_once(SIMPLE_TEST . 'unit_tester.php');
-require_once(SIMPLE_TEST . 'reporter.php');
-
-
-class otpauthHtmlReporter extends HTMLReporter {
-    function paintPass($message) {
-        parent::paintPass($message);
-        $breadcrumb = $this->getTestList();
-
-        print "<font color='green'>Pass: </font>".$breadcrumb[1]."     ".
-               "<font color='gray'>".$this->_htmlEntities($message)."</font><br/>";
+    if (! defined('SIMPLE_TEST')) {
+        define('SIMPLE_TEST', './simpletest/');
     }
-
-
-
-    function paintFooter($test_name) {
-        $colour = ($this->getFailCount() + $this->getExceptionCount() > 0 ? "red" : "green");
-
-        print "<table border=0 width=100%>";
-        print "<tr><td bgcolor='lightgray' border='1'>";
-        print "<font color='green'> <b>".$this->getPassCount()."</b> passes</font>, ";
-        print "<font color='red'> <b>".$this->getFailCount()."</b> fails</font>, and ";
-        print "<font color='red'> <b>".$this->getExceptionCount()."</b> unexpected exceptions.</font>";
-        print "</table>";
-//        print "</td></tr></table>";
-/*
-        print "<div style=\"";
-        print "padding: 8px; margin-top: 1em; background-color: $colour; color: white;";
-        print "\">";
-        print "<strong>" . $this->getPassCount() . "</strong> passes, ";
-        print "<strong>" . $this->getFailCount() . "</strong> fails and ";
-        print "<strong>" . $this->getExceptionCount() . "</strong> exceptions.";
-        print "</div>\n";
-*/
+     
+    require_once(SIMPLE_TEST . 'unit_tester.php');
+    require_once(SIMPLE_TEST . 'reporter.php');
+     
+     
+    class otpauthHtmlReporter extends HTMLReporter {
+        function paintPass($message) {
+            parent::paintPass($message);
+            $breadcrumb = $this->getTestList();
+             
+            print "<font color='green'>Pass: </font>".$breadcrumb[1]."     ". "<font color='gray'>".$this->_htmlEntities($message)."</font><br/>";
+        }
+         
+         
+         
+        function paintFooter($test_name) {
+            $colour = ($this->getFailCount() + $this->getExceptionCount() > 0 ? "red" : "green");
+             
+            print "<table border=0 width=100%>";
+            print "<tr><td bgcolor='lightgray' border='1'>";
+            print "<font color='green'> <b>".$this->getPassCount()."</b> passes</font>, ";
+            print "<font color='red'> <b>".$this->getFailCount()."</b> fails</font>, and ";
+            print "<font color='red'> <b>".$this->getExceptionCount()."</b> unexpected exceptions.</font>";
+            print "</table>";
+            //        print "</td></tr></table>";
+            /*
+            print "<div style=\"";
+            print "padding: 8px; margin-top: 1em; background-color: $colour; color: white;";
+            print "\">";
+            print "<strong>" . $this->getPassCount() . "</strong> passes, ";
+            print "<strong>" . $this->getFailCount() . "</strong> fails and ";
+            print "<strong>" . $this->getExceptionCount() . "</strong> exceptions.";
+            print "</div>\n";
+            */
+        }
+         
+        /**
+        *    Paints the top of the web page setting the
+        *    title to the name of the starting test.
+        *    @param string $test_name      Name class of test.
+        *    @access public
+        */
+        function paintHeader($test_name) {
+            $this->sendNoCacheHeaders();
+            print "<br/><br/>";
+            print "<h1>$test_name</h1>\n";
+            flush();
+        }
+         
     }
-
-    /**
-     *    Paints the top of the web page setting the
-     *    title to the name of the starting test.
-     *    @param string $test_name      Name class of test.
-     *    @access public
-     */
-    function paintHeader($test_name) {
-        $this->sendNoCacheHeaders();
-        print "<br/><br/>";
-        print "<h1>$test_name</h1>\n";
-        flush();
-    }
-
-}
-
-
+     
+     
 ?>
