dca1fd289b89d21b34ce64d8b7530d31c7bedea1
james.barkley <james.barkley@f9940d4a-9f55-0410-9c0b-ef6443a9687f>
2009-01-15 04:32:06 +0000

    beautified

--- demo/logout.php
+++ demo/logout.php
@@ -1,18 +1,18 @@
 <?php
-  require_once('db_functions.php');
-  require_once('user_functions.php');
-
-  //attempt to retrieve user session
-  $session = user_getsession();
-
-  /* if user has not logged in at all, send to login page */
-  if (!$session) { 
-    header("Location: login.php");
-    exit();
-  } else {
-    logout($session['user_id']);
-  }
-
+    require_once('db_functions.php');
+    require_once('user_functions.php');
+     
+    //attempt to retrieve user session
+    $session = user_getsession();
+     
+    /* if user has not logged in at all, send to login page */
+    if (!$session) {
+        header("Location: login.php");
+        exit();
+    } else {
+        logout($session['user_id']);
+    }
+     
     header("Location: login.php");
     exit();
 ?>
